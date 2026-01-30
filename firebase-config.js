fef4248a2a718b15e587072e824bb53750dec321
XinRong Admin <admin@xinrong.com>
2026-01-30 16:54:49 +0800

    v0.8 Release

---
+++ firebase-config.js
@@ -0,0 +1,15 @@
+// Firebase Configuration
+// TODO: Replace with your actual Firebase project configuration
+const firebaseConfig = {
+    apiKey: "YOUR_API_KEY",
+    authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
+    projectId: "YOUR_PROJECT_ID",
+    storageBucket: "YOUR_PROJECT_ID.appspot.com",
+    messagingSenderId: "YOUR_SENDER_ID",
+    appId: "YOUR_APP_ID"
+};
+
+// Initialize Firebase
+// Note: We'll include the Firebase SDKs in the main HTML file before this
+// const app = firebase.initializeApp(firebaseConfig);
+// const db = firebase.firestore(app);
